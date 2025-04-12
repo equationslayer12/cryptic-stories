75af0e1e6e40bddc708902af97fcdd953c250438
equationslayer12 <roy0708usa@gmail.com>
2025-04-12 12:41:17 +0300

    fetch story contents from url

--- src/pages/story/StoryPage.tsx
+++ src/pages/story/StoryPage.tsx
@@ -1,4 +1,11 @@
+import { getStories } from '../../stories/stories';
+
 function StoryPage() {
+  const searchParams = new URLSearchParams(window.location.search);
+  const storyId = searchParams.get('id') || '0';
+
+  const storyContent = getStories()[storyId];
+
   return (
     <div>
       <h1>Welcome to the Story Page!</h1>
